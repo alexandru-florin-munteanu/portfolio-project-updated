96fa8bc1d7b79c2ba2caa5f166f629408ad84901
alexandru-florin-munteanu <87570210+alexandru-florin-munteanu@users.noreply.github.com>
2022-02-03 01:17:33 +0200

    Responsive design redo

--- src/Footer.js
+++ src/Footer.js
@@ -2,18 +2,18 @@ import React from "react";
 import "./Footer.css";
 function Footer() {
   return (
-    <div className="footer">
-      <div className="footer__quote">
+    <footer className="footer">
+      <blockquote className="footer__quote">
         <p className="quote">“ Paths are made by walking ”</p>
         <p className="author"> - Franz Kafka</p>
-      </div>
+      </blockquote>
       <div className="footer__logo">
         <div className="footer__image"></div>
         <div className="footer__copyright">
           <p>&copy; Copyright by Alexandru Munteanu. Personal Portfolio</p>
         </div>
       </div>
-    </div>
+    </footer>
   );
 }
 
